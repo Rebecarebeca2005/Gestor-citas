bc9c4314a5bb154b045b443af066bec958f838f2
Rebeca Miguel Sancho <rebeca.migsan@educa.jcyl.es>
2026-04-21 20:34:49 +0200

    prueba 2 despliegue

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Funciona");
+  res.send("FUNCIONA");
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log("Servidor listo");
+app.get("/test", (req, res) => {
+  res.send("TEST OK");
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).send("Error");
+  console.error(err);
+  res.status(500).send("Error interno");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Servidor en puerto " + port);
 });
