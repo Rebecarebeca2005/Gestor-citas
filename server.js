8f9401d90f9ade4b374a9256ddd1e32c883f2849
Rebeca Miguel Sancho <rebeca.migsan@educa.jcyl.es>
2026-04-21 20:50:33 +0200

    prueba (final espero)

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
+// Archivos estáticos (CSS, JS, imágenes)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Página principal
 app.get("/", (req, res) => {
-  res.send("FUNCIONA");
+  res.sendFile(path.join(__dirname, "views", "home.html"));
 });
 
-app.get("/test", (req, res) => {
-  res.send("TEST OK");
-});
-
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send("Error interno");
-});
-
+// Puerto Railway
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log("Servidor en puerto " + port);
 });
